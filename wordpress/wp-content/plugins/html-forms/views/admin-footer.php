037c8b24e186670b1cc2dd80f10e636cf15855fa
hui2000ji <391085296@qq.com>
2020-06-08 22:40:43 +0800

    Initial commit

--- wordpress/wp-content/plugins/html-forms/views/admin-footer.php
+++
@@ -1,4 +0,0 @@
-<div class="hf-small-margin">
-	<p><strong><?php _e( 'Looking for help?', 'html-forms' ); ?></strong>
-		<?php echo sprintf( __('Check out the <a href="%s">HTML Forms knowledge base</a>', 'html-forms' ), 'https://kb.htmlforms.io/#utm_source=wp-plugin&utm_medium=html-forms&utm_campaign=admin-footer' ); ?>.</p>
-</div>
