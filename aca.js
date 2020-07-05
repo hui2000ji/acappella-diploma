06c18eba91e2dacf6d0bba5de4da04382bfa10e1
hui2000ji <391085296@qq.com>
2020-07-05 13:42:55 +0800

    add crop

--- aca.js
+++ aca.js
@@ -1,23 +1,53 @@
 "use strict"
 
-let getClearedCtx = function() {
-    let canvas = document.getElementById('canvas')
-    let ctx = canvas.getContext('2d')
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    return {canvas, ctx}
+let ctx = null
+let canvas = document.getElementById('canvas')
+let lastUploadedImgFile = ''
+let imgLoaded = false
+
+let initCtx = function(clear = true) {
+    ctx = canvas.getContext('2d')
+    if (clear)
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
 }
 
 let showCanvas = function (canvas) {
     canvas.toBlob(function(blob) {
         let targetImg = document.getElementById('final-image')
+        if (targetImg.src)
+            URL.revokeObjectURL(targetImg.src)
         targetImg.src = URL.createObjectURL(blob)
     })
 }
 
+let drawImageThenShowCanvas = function(img, ctx) {
+    ctx.drawImage(img, 2885, 1060, 569, 758)
+    showCanvas(ctx.canvas)
+}
+
+let croppie = new Croppie(document.getElementById('cropper'), {
+    viewport: { width: '240px', height: '320px' },
+    showZoomer: false,
+    enableOrientation: true
+});
+
+let cropImg = function() {
+    croppie.result('blob').then(function(blob) {
+        let img = document.getElementById('cropped-photo');
+        if (img.src)
+            URL.revokeObjectURL(img.src)
+        img.src = URL.createObjectURL(blob)
+        img.onload = () => {
+            drawImageThenShowCanvas(img, ctx)
+            imgLoaded = true
+        }
+    });
+}
+
 let drawCanvas = function() {
-    let {canvas, ctx} = getClearedCtx()
     let progress = 0
-    let deferPainting = false
+    
+    initCtx()
 
     let backgroundImg = document.getElementById('background-img')
     ctx.drawImage(backgroundImg, 0, 0, 3780, 2598)
@@ -68,19 +98,46 @@ let drawCanvas = function() {
     let photoInput = document.getElementById('input-photo-button')
     if (photoInput.value) {
         progress += 25
-        let img = new Image();
-        img.src = URL.createObjectURL(photoInput.files[0]);
-        deferPainting = true
-        img.addEventListener('load', function() {
-            ctx.drawImage(img, 2885, 1060, 569, 758)
-            showCanvas(canvas)
-        }, false);
+        let img = document.getElementById('photo')
+
+        if (lastUploadedImgFile !== photoInput.files[0]) {
+            imgLoaded = false
+            lastUploadedImgFile = photoInput.files[0]
+            if (img.src)
+                URL.revokeObjectURL(img.src)
+            img.src = URL.createObjectURL(photoInput.files[0]);
+    
+            img.onload =  function() {
+                let cropElem = document.getElementById('cropper-display')
+                let imgScale = img.width / img.height
+                if (imgScale < 0.72 || imgScale > 0.78) {
+                    cropElem.style.display = ''
+                    croppie.bind({
+                        url: img.src
+                    }).then(() => cropImg())
+                } else {
+                    cropElem.style.display = 'none'
+                    drawImageThenShowCanvas(img, ctx)
+                    imgLoaded = true
+                }
+            }
+        } else if (imgLoaded) {
+            let imgScale = img.width / img.height
+            if (imgScale < 0.72 || imgScale > 0.78) {
+                let croppedImg = document.getElementById('cropped-photo')
+                drawImageThenShowCanvas(croppedImg, ctx)
+            } else drawImageThenShowCanvas(img, ctx)
+        }
     }
 
-    if (!deferPainting)
+    if (!photoInput.value)
         showCanvas(canvas)
     let progressBarElem = document.getElementById('progress-bar')
     progressBarElem.style.width = progress + '%'
+}
+
+let rotateImg = function(deg) {
+    croppie.rotate(deg)
 }
 
 window.onload = () => drawCanvas()
